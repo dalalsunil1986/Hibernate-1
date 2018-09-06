fe3f1ed30c04406eed73a5248ad2472336a97641
karra sankar <karrasankar158@gmail.com>
2018-09-06 17:15:34 +0530

    working with ses.load method

---
+++ HbProj3-Get-Working-with-ses.load()-case2/src/com/nt/test/LoadObjectTest.java
@@ -0,0 +1,74 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.nt.entity.InsurencePolicy;
+
+public class LoadObjectTest {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg=null;
+		SessionFactory factory=null;
+		Session ses=null;
+		InsurencePolicy policy=null;
+		
+		//Bootstrapping Hibernate
+		
+		cfg=new Configuration();
+		
+	//Hold hibernate configuration(cfg) file and Hibernate mapping file
+		
+		//read Cfg ,mapping files
+		
+		
+		cfg=cfg.configure("/com/nt/cfgs/hibernate.cfg.xml");
+		
+		//Build SessionFactory Object
+		
+		//create SessionFactory Object
+		
+				factory=cfg.buildSessionFactory();
+				
+				//create Session
+				
+				ses=factory.openSession();
+			try {
+				//Get Object
+				
+				policy=ses.load(InsurencePolicy.class, 101);
+				
+				
+					System.out.println("\n Record or Object found, Details::\n");
+				
+					System.out.println(policy.getPolicyId()+"\t"+policy.getPolicyName()+"\t"+policy.getPolicyHolderName()+"\t"+policy.getTenure());
+				
+			}//try
+			
+			catch(HibernateException he) {
+				
+				System.out.println("\n No Record or object Found::\n");
+				
+				he.printStackTrace();
+				
+			}//catch
+			
+			finally {
+				
+				//close objects
+				
+				ses.close();
+				
+				factory.close();
+				
+			}//finally
+				
+		
+		
+				
+	}//main method
+
+}//class
