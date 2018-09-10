727b86a35a57f9d50ed5fbcde2a4454cee9b2d6d
karra sankar <karrasankar158@gmail.com>
2018-09-10 15:45:07 +0530

    saveOrUpdate-dynamic-update

---
+++ HBProj7-SaveOrUpdate2-dynamic-update/src/com/nt/entity/BrandFactory.java
@@ -0,0 +1,20 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@SuppressWarnings("serial")
+@Data
+
+public class BrandFactory implements Serializable {
+	
+	private long custMobileNo;
+	
+	private String custName;
+	
+	private String custAddr;
+	
+	private int rewardPoints;
+
+}
