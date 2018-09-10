1e6032efe541fac262baac42b42072ca3903b1c4
karra sankar <karrasankar158@gmail.com>
2018-09-10 15:38:34 +0530

    saveOrUpdate

---
+++ HBProj7-SaveOrUpdate/src/com/nt/test/SaveOrUpdattest1.java
@@ -0,0 +1,69 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.nt.entity.BrandFactory;
+import com.nt.utility.HibernateUtil;
+
+public class SaveOrUpdattest1 {
+
+	public static void main(String[] args) {
+		
+		Session ses=null;
+		BrandFactory factory=null;
+		Transaction tx=null;
+		boolean flag=false;
+		
+		//Get Session 
+		
+		ses=HibernateUtil.getSession();
+		
+		try {
+		
+		tx=ses.beginTransaction(); 
+		
+		factory=new BrandFactory() ;
+		
+		factory.setCustMobileNo(9999999999L);
+		factory.setCustName("Sankar");
+		factory.setCustAddr("AP");
+		factory.setRewardPoints(8);
+		
+		flag=true;
+		
+		//saveOrUpdate the Object
+		
+		ses.saveOrUpdate(factory);
+			
+		}//try
+		
+		catch(HibernateException he) {
+			flag=false;
+		
+		}
+	
+		finally {
+			if(flag) {
+				
+				tx.commit();
+			System.out.println("Object Updated");
+			
+		}
+		else {
+			
+			tx.rollback();
+			System.out.println("Object is Not Updated");
+			
+		}
+			
+			//Close Objects
+			HibernateUtil.closeSession(ses);
+			HibernateUtil.closeSessionFactory();
+		}//finally	
+		
+		
+	}//main
+
+}//class
