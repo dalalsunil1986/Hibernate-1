0175fb6b595551dcc48776fe9a0539dcf2d2001b
karra sankar <karrasankar158@gmail.com>
2018-09-23 13:23:24 +0530

    CUSTOMGENERATOR--LUCKYCUPONNUMBER

---
+++ HBProj15CustomGenerator-LuckyCuponNumber/src/com/nt/generator/CustomCuponIdGenerator.java
@@ -0,0 +1,39 @@
+package com.nt.generator;
+
+import java.io.Serializable;
+import java.util.Random;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+public class CustomCuponIdGenerator implements IdentifierGenerator {
+	
+	
+
+	public CustomCuponIdGenerator() {
+		
+		System.out.println("customIdGenerator.customIdGenerator()");
+		
+	}
+
+	@Override
+	
+	public Serializable generate(SharedSessionContractImplementor session, Object obj) throws HibernateException {
+		
+		System.out.println("customIdGenerator.generate()");
+		
+		Random rad=null;
+		
+		long val=0;
+		
+		//Generate Random Number
+		
+		rad=new Random();
+		
+		val=rad.nextLong();
+		
+		return val;
+	}
+
+}
