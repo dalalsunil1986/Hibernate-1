ff268275d3e46fdd98099096738b88a0de28113f
karra sankar <karrasankar158@gmail.com>
2018-09-23 13:20:53 +0530

    GENERATOR--select

---
+++ HBPorj14-Generators-select/src/com/nt/entity/Student.java
@@ -0,0 +1,30 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@SuppressWarnings("serial")
+
+
+@Data
+
+public class Student implements Serializable {
+	
+	private String sno;
+	
+	private String sname;
+	
+	private String sadd;
+	
+	private String email;
+
+	public Student() {
+	
+		System.out.println("Student:: 0-parameter Constructor");
+		
+	}
+	
+	
+
+}
