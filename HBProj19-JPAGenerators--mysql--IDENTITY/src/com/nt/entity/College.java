4bbc7e1b60a6b2603ae027067cdecb79a6aee4b6
karra sankar <karrasankar158@gmail.com>
2018-09-26 23:08:30 +0530

    JPA--Generators-IDENTITY-mysql

---
+++ HBProj19-JPAGenerators--mysql--IDENTITY/src/com/nt/entity/College.java
@@ -0,0 +1,37 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@SuppressWarnings("serial")
+
+@Entity
+
+@Data
+
+public class College implements Serializable {
+	
+	@Id
+	
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	private int  id;
+	
+	private String name;
+	
+	private String location;
+	
+	private int strength;
+	
+	private String grade;
+
+	
+}
