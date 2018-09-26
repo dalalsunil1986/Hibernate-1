b5371aed9eca64bfbe5147d5880983bb65c452f1
karra sankar <karrasankar158@gmail.com>
2018-09-26 23:04:17 +0530

    HBGenericGenerators--sequence-oracle

---
+++ HBProj18-HBGenericGenerators--sequence-oracle2/src/com/nt/entity/College.java
@@ -0,0 +1,42 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+import lombok.Data;
+
+
+@SuppressWarnings("serial")
+@Entity
+
+@Data
+
+public class College implements Serializable {
+	
+	@Id
+	
+	@GenericGenerator(name="gen1",strategy="sequence")
+	
+	
+	@GeneratedValue(generator="gen1")
+	
+	private int  id;
+	
+	private String name;
+	
+	private String location;
+	
+	private int strength;
+	
+	private String grade;
+
+	
+}
