e2cd49648d2d421f311ac23cfbcd5524dff7c011
karra sankar <karrasankar158@gmail.com>
2018-09-26 22:59:04 +0530

    HBGenericGenerators--increment-oracle

---
+++ HBProj18-HBGenericGenerators--increment-oracle/src/com/nt/entity/College.java
@@ -0,0 +1,41 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+import lombok.Data;
+
+
+@SuppressWarnings("serial")
+@Entity
+
+@Data
+
+public class College implements Serializable {
+	
+	@Id
+	
+	@GenericGenerator(name="gen1",strategy="increment")
+	
+	@GeneratedValue(generator="gen1")
+	
+	private int  id;
+	
+	private String name;
+	
+	private String location;
+	
+	private int strength;
+	
+	private String grade;
+
+	
+}
