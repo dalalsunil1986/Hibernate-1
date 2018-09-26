7e28922fb68593218603671733ddb0999a5ffe50
karra sankar <karrasankar158@gmail.com>
2018-09-26 22:56:21 +0530

    JPA-Annotation--table-column-optional-Transient--means-certainPropertyNotParticifiented

---
+++ HBProj17-Basic-JPA-Annotation--table-column-optional-Transient--means-certainPropertyNotParticifiented/src/com/nt/test/AnnoHBSaveTest.java
@@ -0,0 +1,93 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.nt.entity.College;
+import com.nt.utility.HibernateUtil;
+
+public class AnnoHBSaveTest {
+
+	public static void main(String[] args) {
+		
+		Session ses=null;
+		
+		Transaction tx=null;
+		
+		College clg=null;
+		
+		boolean flag=false;
+		
+		//Get Session 
+		
+		ses=HibernateUtil.getSession();
+		
+		try {
+			
+			tx=ses.beginTransaction();
+			
+			clg=new College();
+			
+			//here every compilation change id and name
+			
+			clg.setId(7889);
+			
+			clg.setName("Sankar101");
+			
+			clg.setLocation("razole");
+			
+			clg.setStrength(200);
+			
+			clg.setGrade("A");
+			
+			ses.save(clg);
+			
+			flag=true;
+			
+		}//try
+		
+		catch(HibernateException he) {
+			
+			flag=false;
+			
+			he.printStackTrace();
+		}
+		
+		catch(Exception e) {
+			
+			flag=false;
+			
+			e.printStackTrace();
+		}
+		
+		finally {
+			
+			if(flag) {
+				
+				tx.commit();
+				
+				System.out.println("Object is Saved");
+				
+			}
+			
+			else {
+				
+				tx.rollback();
+				
+				System.out.println("Object is not Saved");
+				
+			}
+			
+			//close Objects
+			
+			HibernateUtil.closeSession(ses);
+			
+			HibernateUtil.closeSessionFactory();
+			
+		}
+		
+
+	}
+
+}
