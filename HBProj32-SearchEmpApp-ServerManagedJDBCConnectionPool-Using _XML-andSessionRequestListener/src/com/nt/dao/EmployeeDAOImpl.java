a1214b2c9cdf890094622f10e10466fcf9bee123
karra sankar <karrasankar158@gmail.com>
2018-10-09 19:15:49 +0530

    servler managed connection pool -->session request listener

---
+++ HBProj32-SearchEmpApp-ServerManagedJDBCConnectionPool-Using _XML-andSessionRequestListener/src/com/nt/dao/EmployeeDAOImpl.java
@@ -0,0 +1,30 @@
+package com.nt.dao;
+
+import org.hibernate.Session;
+
+import com.nt.entity.Employee;
+import com.nt.utility.HibernateUtil;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	@Override
+	public Employee findEmployee(int eno) {
+	
+		Session ses=null;
+		
+		Employee emp=null;
+		
+		//Get Session 
+		
+		ses=HibernateUtil.getSession();
+		
+		System.out.println("Ses object Hashcodde::"+ses.hashCode());
+		
+		//LoadObject
+		
+		emp=ses.get(Employee.class,eno);
+		
+		return emp;
+	}
+
+}
---
+++ HBProj32-SearchEmpApp-ServerManagedJDBCConnectionPool-Using _XML-andSessionRequestListener/src/com/nt/listener/Session_RequestListener.java
@@ -0,0 +1,29 @@
+package com.nt.listener;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServletRequest;
+
+import com.nt.utility.HibernateUtil;
+
+@WebListener
+
+public class Session_RequestListener implements ServletRequestListener {
+	
+	HttpServletRequest hsr=null;
+	
+	@Override
+	public void requestDestroyed(ServletRequestEvent sre) {
+		
+		System.out.println("Session_RequestListener.requestDestroyed()");
+		
+		//Get Request Object
+		
+		hsr=(HttpServletRequest) sre.getServletRequest();
+		
+		HibernateUtil.closeSession();
+		
+	}
+
+}
